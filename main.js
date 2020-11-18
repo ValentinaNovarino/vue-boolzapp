421a334bb27a09fb33c7188e0528f19d818ab7c8
ValentinaNovarino <valenovarino@gmail.com>
2020-11-18 09:35:07 +0100

    aggiunto autoscroll

--- main.js
+++ main.js
@@ -14,17 +14,20 @@ var app = new Vue (
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Hai portato a spasso il cane?',
-                            status: 'sent'
+                            status: 'sent',
+                            visibility: 'hidden-dropdown-menu'
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Ricordati di dargli da mangiare',
-                            status: 'sent'
+                            status: 'sent',
+                            visibility: 'hidden-dropdown-menu'
                         },
                         {
                             date: '10/01/2020 16:15:22',
                             message: 'Tutto fatto!',
-                            status: 'received'
+                            status: 'received',
+                            visibility: 'hidden-dropdown-menu'
                         }
                     ],
                 },
@@ -36,17 +39,20 @@ var app = new Vue (
                         {
                             date: '20/03/2020 16:30:00',
                             message: 'Ciao come stai?',
-                            status: 'sent'
+                            status: 'sent',
+                            visibility: 'hidden-dropdown-menu'
                         },
                         {
                             date: '20/03/2020 16:30:55',
                             message: 'Bene grazie! Stasera ci vediamo?',
-                            status: 'received'
+                            status: 'received',
+                            visibility: 'hidden-dropdown-menu'
                         },
                         {
                             date: '20/03/2020 16:35:00',
                             message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                            status: 'sent'
+                            status: 'sent',
+                            visibility: 'hidden-dropdown-menu'
                         }
                     ],
                 },
@@ -58,15 +64,20 @@ var app = new Vue (
                         {
                             date: '28/03/2020 10:10:40',
                             message: 'La Marianna va in campagna',
-                            status: 'received'
+                            status: 'received',
+                            visibility: 'hidden-dropdown-menu'
                         },
                         {
                             date: '28/03/2020 10:20:10',
                             message: 'Sicuro di non aver sbagliato chat?',
-                            status: 'sent'
+                            status: 'sent',
+                            visibility: 'hidden-dropdown-menu'
                         },
                         {
-                            date: '28/03/2020 16:15:22',message: 'Ah scusa!',status: 'received'
+                            date: '28/03/2020 16:15:22',
+                            message: 'Ah scusa!',
+                            status: 'received',
+                            visibility: 'hidden-dropdown-menu'
                         }
                     ],
                 },
@@ -78,12 +89,14 @@ var app = new Vue (
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Lo sai che ha aperto una nuova pizzeria?',
-                            status: 'sent'
+                            status: 'sent',
+                            visibility: 'hidden-dropdown-menu'
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Si, ma preferirei andare al cinema',
-                            status: 'received'
+                            status: 'received',
+                            visibility: 'hidden-dropdown-menu'
                         },
 
                     ],
@@ -91,31 +104,49 @@ var app = new Vue (
             ]
         },
         methods: {
+            // funzione per cambiare chat al click
             changeChat(i) {
                 this.contactIndex = i;
+                this.autoscroll();
             },
-            addMessage(newItem) {
+            // funzione per scrivere nuovo messaggio tramite input
+            addMessage() {
+                // creo oggetto per il nuovo messaggio
                 var newItem = {
                     date:'10/01/2020 15:50:00' ,
                     message: '',
-                    status: 'sent'
+                    status: 'sent',
+                    visibility: 'hidden-dropdown-menu'
                 }
-
+                // riempo stringa di message con testo scritto nell'input
                 newItem.message = this.newMessage;
-
+                // faccio push nell'array messages del nuovo oggetto
                 this.contacts[this.contactIndex].messages.push(newItem);
-
+                // svuoto la stringa dell'input
                 this.newMessage = '';
-
+                // faccio scroll della finestra della chat nvocando la funzione
+                this.autoscroll();
+                // creo una timing function che dopo 1 secondo dall'invio del messaggio genera un mesaggio di risposta
                 setTimeout(() => {
+                    // creo oggetto per il messaggio di risposta
                     var newAnswer = {
                         date:'10/01/2020 15:50:00' ,
                         message: 'Ok',
-                        status: 'received'
+                        status: 'received',
+                        visibility: 'hidden-dropdown-menu'
                     }
-
+                    // faccio push nell'array messages del nuovo oggetto
                     this.contacts[this.contactIndex].messages.push(newAnswer);
+
+                    // faccio scroll della finestra della chat invocando la funzione
+                    this.autoscroll();
                 }, 1000);
+            },
+            autoscroll(){
+                Vue.nextTick(function() {
+                    let chatContainer = document.getElementsByClassName('chat')[0]
+                    chatContainer.scrollTop = chatContainer.scrollHeight;
+                });
             },
             searchContact() {
                 // variabile per salvare il valore scritto dall'utente
@@ -132,10 +163,14 @@ var app = new Vue (
                     // console.log(item.visible);
                 });
             },
+            // funzione per eliminare messaggio al click su delete message
             deleteMessage(i) {
+                // cancello il messaggio in base all'indice del messaggio preso in considerazione
                 this.contacts[this.contactIndex].messages.splice(i, 1);
-            }
-
+            },
+        },
+        mounted: function() {
+            this.autoscroll();
         },
     }
 );
